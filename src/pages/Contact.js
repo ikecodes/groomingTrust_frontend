b944cb9db484b1c768b20a1006f974c607d27f36
Onuorah Ike <ikecodes@gmail.com>
2022-03-11 19:37:54 +0100

    finished contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { RiSendPlaneFill } from 'react-icons/ri';
+import { MdLocationOn } from 'react-icons/md';
+import { BsTelephoneFill } from 'react-icons/bs';
 import { Form } from 'react-bootstrap';
 import styled from 'styled-components';
 import colors from '../constants/colors';
@@ -8,46 +11,123 @@ const Contact = () => {
   return (
     <Layout header='contact us'>
       <Container>
-        <h2 className='my-3 text-capitalize'>we are here for you</h2>
+        <h1 className='my-3 text-capitalize'>we are here for you</h1>
         <p className='mb-5'>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, neque
           accusantium perferendis est unde soluta magni? Dolor, provident quas!
           Quas totam distinctio veritatis hic pariatur tempora voluptas dolores
           nam saepe?
         </p>
-        <div className='row justify-content-center'>
-          <div className='col-lg-6'>
-            <Form className='bg-light p-3 rounded'>
-              <Form.Group className='mb-3' controlId='formGroupName'>
-                <Form.Label>Name</Form.Label>
-                <Form.Control type='text' placeholder='Enter name' />
-              </Form.Group>
-              <Form.Group className='mb-3' controlId='formGroupEmail'>
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type='email' placeholder='Enter email' />
-              </Form.Group>
-              <Form.Group className='mb-3' controlId='formGroupMessage'>
-                <Form.Label>Message</Form.Label>
+        <ContactContainer className='row justify-content-center shadow-lg'>
+          <ContactDetails className='col-lg-6 bg-light p-4 m-0 h-100'>
+            <h2 className='text-capitalize m-0'>contact us</h2>
+            <HeaderLine className='mb-1' />
+            <p>We are open to any suggestion or just to have a chat</p>
+            <ul className='mt-5'>
+              <li className='mb-5 d-flex gap-3 align-items-center'>
+                <MdLocationOn
+                  size={30}
+                  color={colors.primary}
+                  className='flex-shrink-0'
+                />
+                <span>
+                  Address:{' '}
+                  <Detail>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit
+                  </Detail>
+                </span>
+              </li>
+              <li className='mb-5 d-flex gap-3 align-items-center'>
+                <RiSendPlaneFill size={30} color={colors.primary} />
+                <span>
+                  Email: <Detail>example@gmail.com</Detail>
+                </span>
+              </li>
+              <li className='mb-5 d-flex gap-3 align-items-center'>
+                <BsTelephoneFill size={30} color={colors.primary} />
+                <span>
+                  Phone: <Detail>080354677797</Detail>
+                </span>
+              </li>
+            </ul>
+          </ContactDetails>
+          <ContactForm className='col-lg-6 p-0'>
+            <Form className='p-3 m-0'>
+              <div className='mb-3 form-group'>
+                <label htmlFor='name' className='form-label'>
+                  Full name
+                </label>
+                <input
+                  type='text'
+                  name='name'
+                  className='form-control rounded-0'
+                  placeholder='Enter your name'
+                />
+              </div>
+              <div className='mb-3 form-group'>
+                <label htmlFor='email' className='form-label'>
+                  Email
+                </label>
+                <input
+                  type='email'
+                  name='email'
+                  className='form-control rounded-0'
+                  placeholder='Email Address'
+                />
+              </div>
+              <div className='mb-3 form-group'>
+                <label htmlFor='message' className='form-label'>
+                  Message
+                </label>
                 <textarea
-                  name=''
-                  id=''
+                  name='message'
                   cols='30'
                   rows='5'
-                  className='form-control'
+                  className='form-control rounded-0 borded-0'
+                  placeholder='Your message for us...'
                 ></textarea>
-              </Form.Group>
-              <div className='text-end'>
-                <Button primary title='submit' />
+              </div>
+              <div className='text-start'>
+                <Button primary title='submit' norounded />
               </div>
             </Form>
-          </div>
-        </div>
+          </ContactForm>
+        </ContactContainer>
       </Container>
     </Layout>
   );
 };
 const Container = styled.div`
   color: ${colors.textColor};
+`;
+const HeaderLine = styled.div`
+  background-color: ${colors.primary};
+  height: 0.2rem;
+  width: 5rem;
+  border-radius: 10px;
+`;
+const ContactContainer = styled.div`
+  height: 100%;
+`;
+
+const ContactDetails = styled.div`
+  /* background-color: ${colors.white}; */
+  color: ${colors.textColor};
+  height: 100%;
+`;
+const ContactForm = styled.div`
+  background-color: ${colors.tertiary};
+  background-image: linear-gradient(
+    to top right,
+    ${colors.tertiary},
+    ${colors.primary}
+  );
+  color: ${colors.white};
+  height: 100%;
+`;
+
+const Detail = styled.span`
+  color: #696969;
 `;
 
 export default Contact;
