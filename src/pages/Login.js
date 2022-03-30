d6aa0faa4d329da4feefee3133a53db813b8e6f6
Onuorah Ike <ikecodes@gmail.com>
2022-03-30 20:28:14 +0100

    setup auth state change

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { signInWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
 import { auth } from '../firebase';
