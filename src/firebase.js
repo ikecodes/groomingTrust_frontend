11b6d006396249aa1592d9a07f9d6e82e9df245a
Onuorah Ike <ikecodes@gmail.com>
2022-03-30 19:43:50 +0100

    moved keys to env file

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCErQF3fw_tG-QGBJztNa50CqdBRGZd-FA',
-  authDomain: 'grooming-endowment-trust.firebaseapp.com',
-  projectId: 'grooming-endowment-trust',
-  storageBucket: 'grooming-endowment-trust.appspot.com',
-  messagingSenderId: '29710503166',
-  appId: '1:29710503166:web:0d7f0c3f355cf8708329af',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
